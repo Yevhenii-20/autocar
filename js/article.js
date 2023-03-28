a49f605f8144acce41191d7649fc26e1ff227eb3
Yevhenii-20 <60322582+Yevhenii-20@users.noreply.github.com>
2023-03-28 15:17:46 +0300

    Add files via upload

---
+++ js/article.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $(".burger-menu ").on("click",".bar",function(e){
+        $(".bar").toggleClass('change');
+        $('.right-header').toggleClass('show');
+        e.preventDefault();
+    });
+
+    $(".header-link_block").on("click",function(){
+      $('.header_submenu').removeClass('show');
+      $(this).children('.header_submenu').toggleClass('show');
+
+  });
+});
